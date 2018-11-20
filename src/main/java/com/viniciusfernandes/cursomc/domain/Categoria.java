52b2e751c96f87a3843b77fa297913a1646cd16c
Vinicius Fernandes <vinicius.f.fernandes@hotmail.com>
2018-11-20 11:57:58 -0200

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/viniciusfernandes/cursomc/domain/Categoria.java
+++ src/main/java/com/viniciusfernandes/cursomc/domain/Categoria.java
@@ -2,9 +2,19 @@ package com.viniciusfernandes.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //strategia de geraçao automatica de ID para criar a tabela no banco
 	private Integer id;
 	private String nome;
 	
