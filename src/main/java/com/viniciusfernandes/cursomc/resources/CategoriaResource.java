a32ec25f96be7c97a28ba211023b3237899239ab
Vinicius Fernandes <vinicius.f.fernandes@hotmail.com>
2018-11-20 22:07:34 -0200

    Criando repository e service para Categoria

--- src/main/java/com/viniciusfernandes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/viniciusfernandes/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,31 @@ package com.viniciusfernandes.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.viniciusfernandes.cursomc.domain.Categoria;
+import com.viniciusfernandes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired //para instanciar automaticamente esse objeto
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) { //PathVariable para o Spring entender que estamos fazendo referencia para o "/{id}"
+		//ResponseEntity - tipo especial que armazena varias informaçoes http para um serviço REST
+		//o <?> quer dizer que pode ser qualquer tipo, qualquer informaçao
 		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritório");
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
 	}
 
 }
